537093949c7b00ef793f3031a3a443edb4a00947
Šimon Brauner <simonbrauner@seznam.cz>
2024-08-26 11:05:45 +0200

    feat(ssh): generate keys script

---
+++ scripts/generate_ssh_keys.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+ssh-keygen -t ed25519 -f ~/.ssh/aisa -C "xbrauner@fi.muni.cz"
+ssh-keygen -t ed25519 -f ~/.ssh/github -C "simonbrauner@seznam.cz"
+ssh-keygen -t ed25519 -f ~/.ssh/gitlab -C "xbrauner@fi.muni.cz"
